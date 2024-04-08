f3c2c71824ef7c7dc3ea9ea04091eeae94362ed3
abirchLBG <>
2024-04-08 16:06:59 +0100

    pathing

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pandas as pd
 import yfinance as yf
 import re
 import numpy as np
+from termcolor import colored
+import sys
 
 
 def get_currency(x):
@@ -12,7 +14,7 @@ def get_currency(x):
 
 
 class Portfolio:
-    def __init__(self, path="portfolio.csv",):
+    def __init__(self, path=sys.path[0] + "/portfolio.csv",):
         self.path = path
         self.df_raw = pd.read_csv(path, usecols=["Investment","Quantity"], index_col=False)
         self.usd_gbp = yf.Ticker("USDGBP=X")
@@ -33,7 +35,9 @@ class Portfolio:
         
         self.df_stocks["Currency"] = ""
         self.df_stocks["Currency"] = self.df_stocks["Ticker"].where(self.df_stocks["Ticker"] != "").apply(get_currency)
-        self.df_stocks["Price Raw"] = self.df_stocks["Ticker"].apply(lambda x: yf.Ticker(x).info["bid"])
+
+
+        self.df_stocks["Price Raw"] = self.df_stocks["Ticker"].apply(lambda x: yf.Ticker(x).info["previousClose"])
         
         self.df_stocks["Price GBP"] = np.nan
         self.df_stocks["Price GBP"] = self.df_stocks["Price GBP"].where(self.df_stocks["Currency"] != "GBP", self.df_stocks["Price Raw"]).round(2)
@@ -47,12 +51,24 @@ class Portfolio:
 
 
     def show_portoflio_totals(self):
-        print("Portfolio:")
-        print(f"£ {self.df_stocks['Total Value GBP'].sum() + self.cash['GBP']:,.2f}")
-        print(f"$ {self.df_stocks['Total Value USD'].sum() + self.cash['USD']:,.2f}")
+        self.total_gbp = self.df_stocks['Total Value GBP'].sum() + self.cash['GBP']
+        self.total_usd = self.df_stocks['Total Value USD'].sum() + self.cash['USD']
+        print("Portfolio totals:")
+        print(colored(f"£ {self.total_gbp:,.2f}", "light_green"))
+        print(colored(f"$ {self.total_usd:,.2f}", "light_green"))
 
+
+    def show_forecast(self, years):
+        print()
+        print(f"{years}Y forecast:")
+        print(f"£ {round(self.total_gbp * 1.09**years, 2):,.2f}")
+        print(f"$ {round(self.total_usd * 1.09**years, 2):,.2f}")
 
 if __name__ == "__main__":
     portfolio = Portfolio()
     portfolio._init_stocks_df()
     portfolio.show_portoflio_totals()
+    # portfolio.show_forecast(10)
+    # portfolio.show_forecast(20)
+    # portfolio.show_forecast(30)
+    # portfolio.show_forecast(40)
